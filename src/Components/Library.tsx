44d2ab082911f42284e3b82305c098dca7268593
Manakal <dp.manakal82@gmail.com>
2021-06-06 22:28:24 +0530

    Added Author Title

--- src/Components/Library.tsx
+++ src/Components/Library.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import WelcomeScreen from "./WelcomeScreen";
+import Authors from "./Authors/Authors";
 
 const Library=()=>{
     return(
         <Container fluid={true} className="Library-Container px-0">
-            <Row className="Library mx-0 px-0">
+            <Row className="Library mx-0">
                 <Col className="px-0">
                     <WelcomeScreen/>
                 </Col>
             </Row>
-            <Row>
-                <Col className="px-0">
-
+            <Row className=" mx-0 mb-5 pb-5" style={{border:'1px solid black '}}>
+                <Col  md={{order: 'first', span: 6}} xs={{order: 'last', span: 12}}>
+                    <h1>books</h1>
+                </Col>
+                <Col className="" md={6} xs={12} style={{border:'1px solid blue '}}>
+                   <Authors/>
                 </Col>
             </Row>
         </Container>
